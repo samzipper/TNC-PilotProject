07c0b804ff88cf14a9df5046713092f73dd06a67
Sam Zipper <samuelczipper@gmail.com>
2018-04-24 15:37:47 -0700

    Tweaks to Navarro_MetData script

--- src/Navarro_MetData.R
+++ src/Navarro_MetData.R
@@ -39,6 +39,7 @@ df.mo <-
   df %>% 
   group_by(year, month) %>% 
   summarize(mo.days = mean(days_in_month(month)),
+            date.mid = mean(date),
             prec.days = sum(is.finite(prec)),
             prec.sum = sum(prec, na.rm=T),
             Tmax.days = sum(is.finite(Tmax)),
@@ -46,8 +47,12 @@ df.mo <-
             Tmin.days = sum(is.finite(Tmin)),
             Tmin.mean = mean(Tmin, na.rm=T))
 
+# make month a factor
+df.mo$month <- factor(df.mo$month, levels=seq(1,12), labels=labs.mo)
+
 ## calculate monthly PET using Hargreaves formulation (temperature-based)
 df.mo$PET <- hargreaves(df.mo$Tmin.mean, df.mo$Tmax.mean, lat=station.outlet.lat, na.rm=T)
+df.mo$defc <- df.mo$PET - df.mo$prec.sum
 
 ## select months to keep for prec and temp
 missing.day.threshold <- 3
@@ -55,8 +60,25 @@ df.mo$prec.keep <- (df.mo$mo.days - df.mo$prec.days) < missing.day.threshold
 df.mo$Tmax.keep <- (df.mo$mo.days - df.mo$Tmax.days) < missing.day.threshold
 df.mo$Tmin.keep <- (df.mo$mo.days - df.mo$Tmin.days) < missing.day.threshold
 
+## summarize everything to monthly means
+df.mo.mean <- 
+  df.mo %>% 
+  group_by(month) %>% 
+  summarize(prec = mean(prec.sum, na.rm=T),
+            Tmax = mean(Tmax.mean, na.rm=T),
+            PET = mean(PET, na.rm=T),
+            defc = PET - prec) %>% 
+  melt(id=c("month"))
+
 ## make some summary plots
-df.mo$month <- factor(df.mo$month, levels=seq(1,12), labels=labs.mo)
+p.mo.mean <-
+  ggplot(df.mo.mean, aes(x=month, y=value)) +
+  geom_hline(yintercept=0, color="gray65") +
+  geom_bar(stat="identity") +
+  facet_wrap(~variable, ncol=1, scales="free_y") 
+# based on this plot: distribute recharge evenly (30 mm/mo) for Dec-Apr
+
+## other plots of individual variables showing distribution
 p.prec <- 
   ggplot(subset(df.mo, prec.keep), aes(x=month, y=prec.sum, group=month)) +
   geom_boxplot() +
@@ -69,6 +91,14 @@ p.PET <-
   geom_boxplot() +
   geom_point(shape=21) +
   scale_y_continuous(name="PET [mm/mo]") +
+  scale_x_discrete(name="Month")
+
+p.defc <- 
+  ggplot(subset(df.mo, Tmax.keep & prec.keep), aes(x=month, y=defc, group=month)) +
+  geom_boxplot() +
+  geom_hline(yintercept=0, color="gray65") +
+  geom_point(shape=21) +
+  scale_y_continuous(name="PET - Precip [mm/mo]") +
   scale_x_discrete(name="Month")
 
 p.Tmax <- 
