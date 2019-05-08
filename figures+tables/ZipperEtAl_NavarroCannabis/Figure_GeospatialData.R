a1746b5ee3f8a3e13e7458565befee3fac567362
Sam Zipper <samuelczipper@gmail.com>
2019-05-08 14:59:35 -0700

    Update map figure

--- figures+tables/ZipperEtAl_NavarroCannabis/Figure_GeospatialData.R
+++ figures+tables/ZipperEtAl_NavarroCannabis/Figure_GeospatialData.R
@@ -48,6 +48,10 @@ df.all <-
   full_join(df.dem, df.dtb, by=c("lon", "lat")) %>% 
   full_join(df.aquifers, by=c("lon", "lat"))
 
+# synthetic pumping wells
+df.wel <- read.table(file.path("modflow", "input", "iwel.txt"), header=T)
+sf.wel <- st_as_sf(df.wel, coords = c("lon", "lat"), crs = st_crs(sf.streams))
+
 # get rid of locations that have NA in any dataset
 # this is primarily the coastal plain in aquifer and dem
 df.all <- df.all[complete.cases(df.all), ]
@@ -57,7 +61,7 @@ p.elev <-
   geom_raster(data=df.all, aes(x=lon, y=lat, fill=dem)) +
   geom_sf(data=sf.basin, color=col.cat.red, fill=NA) +
   geom_sf(data=sf.streams, color="white") +
-  scale_fill_viridis(name="Land Surface\nElevation [m]") +
+  scale_fill_viridis(name="Land Surface\nElevation [m]", breaks=c(0,500,1000)) +
   scale_x_continuous(name="Easting [m]", expand=c(0,0), breaks=map.breaks.x) +
   scale_y_continuous(name="Northing [m]", expand=c(0,0), breaks=map.breaks.y) +
   coord_sf(crs=crs.MODFLOW, datum=crs.MODFLOW) +
@@ -72,6 +76,7 @@ p.DTB <-
   geom_raster(data=df.all, aes(x=lon, y=lat, fill=dtb)) +
   geom_sf(data=sf.basin, color=col.cat.red, fill=NA) +
   geom_sf(data=sf.streams, color="white") +
+  geom_sf(data=sf.wel, shape=21, color="black", size=0.2) +
   scale_fill_viridis(name="Depth To\nBedrock [m]", direction=-1) +
   scale_x_continuous(name="Easting [m]", expand=c(0,0), breaks=map.breaks.x) +
   scale_y_continuous(name="Northing [m]", expand=c(0,0), breaks=map.breaks.y) +
@@ -83,12 +88,22 @@ p.DTB <-
         legend.box.background=element_blank())
 
 p1 <- ggplotGrob(p.elev)
-p2 <- ggplotGrob(p.DTB)
+p2 <- ggplotGrob(p.DTB + 
+                   annotate("text", 
+                            x = min(df.all$lon)+(max(df.all$lon)-min(df.all$lon))*0.85, 
+                            y = min(df.all$lat)+(max(df.all$lat)-min(df.all$lat))*0.95, 
+                            hjust=0.5, vjust=1, label = "Synthetic\nWells", size=3) +
+                   annotate("segment", 
+                            x = min(df.all$lon)+(max(df.all$lon)-min(df.all$lon))*0.8, 
+                            xend = min(df.all$lon)+(max(df.all$lon)-min(df.all$lon))*0.64, 
+                            y = min(df.all$lat)+(max(df.all$lat)-min(df.all$lat))*0.86, 
+                            yend = min(df.all$lat)+(max(df.all$lat)-min(df.all$lat))*0.65,
+                            colour = "black"))
 p <- cbind(p1, p2, size="first")
 p$heights <- unit.pmax(p1$heights, p1$heights)
 
-ggsave(file.path("figures+tables", "ZipperEtAl_NavarroCannabis", "Figure_GeospatialData.pdf"),
-       p, width=190, height=110, units="mm", device=cairo_pdf)
-
 ggsave(file.path("figures+tables", "ZipperEtAl_NavarroCannabis", "Figure_GeospatialData.png"),
        p, width=190, height=110, units="mm")
+
+ggsave(file.path("figures+tables", "ZipperEtAl_NavarroCannabis", "Figure_GeospatialData.pdf"),
+       p, width=190, height=110, units="mm", device=cairo_pdf)
